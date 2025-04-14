e0a80bc9be6aff4cdd9bf41aed85a425979aefa7
Kevin De Ritis <kevinderitis@MacBookPro.fibertel.com.ar>
2025-04-14 17:28:13 -0300

    user id type changed

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const subscriptionSchema = new mongoose.Schema({
   user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+    type: String,
     required: true,
     unique: true
   },
